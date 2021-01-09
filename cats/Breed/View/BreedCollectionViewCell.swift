1b2fdf708f355eee822ef48af80ab1d9e5556a8f
Gabriel <gabrieldr@id.uff.br>
2021-01-08 21:18:44 -0300

    First CustomCell

---
+++ cats/Breed/View/BreedCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  BreedCollectionViewCell.swift
+//  cats
+//
+//  Created by Gabriel Duarte on 07/01/21.
+//
+
+import UIKit
+
+class BreedCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
